33336285663d933089056d75d77a216da3bfcf42
Raphael San <=>
2025-02-04 00:03:12 -0300

    projeto finalizado

---
+++ desafio-controleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+// Classe ParametrosInvalidosException
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
