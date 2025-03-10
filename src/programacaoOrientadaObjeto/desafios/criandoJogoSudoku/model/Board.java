8a75dfe434b1710990df7bb6282df384aedf9dd6
Raphael San <=>
2025-03-10 14:12:33 -0300

    refatorando o board

--- src/programacaoOrientadaObjeto/desafios/criandoJogoSudoku/model/Board.java
+++ src/programacaoOrientadaObjeto/desafios/criandoJogoSudoku/model/Board.java
@@ -9,32 +9,35 @@ import java.util.Collection;
 import java.util.List;
 
 public class Board {
-	private final List<List<Space>> spaces;
 
-	public Board(final List<List<Space>> spaces) {
-		this.spaces = spaces;
-	}
+    private final List<List<Space>> spaces;
 
-	public List<List<Space>> getSpaces() {
-		return spaces;
-	}
+    public Board(final List<List<Space>> spaces) {
+        this.spaces = spaces;
+    }
 
-	public GameStatusEnum getStatus() {
-		if (spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))) {
-			return NON_STARTED;
-		}
-		return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual())) ? INCOMPLETE : COMPLETE;
-	}
+    public List<List<Space>> getSpaces() {
+        return spaces;
+    }
 
-	public boolean hasErrors() {
-		if (getStatus() == NON_STARTED) {
-			return false;
-		}
-		return spaces.stream().flatMap(Collection::stream)
-				.anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
-	}
-	
-	public boolean changeValue(final int col, final int row, final int value){
+    public GameStatusEnum getStatus(){
+        if (spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))){
+            return NON_STARTED;
+        }
+
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual())) ? INCOMPLETE : COMPLETE;
+    }
+
+    public boolean hasErrors(){
+        if(getStatus() == NON_STARTED){
+            return false;
+        }
+
+        return spaces.stream().flatMap(Collection::stream)
+                .anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
+    }
+
+    public boolean changeValue(final int col, final int row, final int value){
         var space = spaces.get(col).get(row);
         if (space.isFixed()){
             return false;
@@ -43,7 +46,8 @@ public class Board {
         space.setActual(value);
         return true;
     }
-	public boolean clearValue(final int col, final int row){
+
+    public boolean clearValue(final int col, final int row){
         var space = spaces.get(col).get(row);
         if (space.isFixed()){
             return false;
@@ -60,4 +64,5 @@ public class Board {
     public boolean gameIsFinished(){
         return !hasErrors() && getStatus().equals(COMPLETE);
     }
+
 }
