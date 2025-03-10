fddd6eeb9e6d3f7b973a089b0e75faea26f334c0
Raphael San <=>
2025-03-10 16:13:13 -0300

    adicionando user interface

---
+++ src/programacaoOrientadaObjeto/desafios/criandoJogoSudoku/ui/custom/button/ResetButton.java
@@ -0,0 +1,14 @@
+package programacaoOrientadaObjeto.desafios.criandoJogoSudoku.ui.custom.button;
+
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JButton {
+
+    public ResetButton(final ActionListener actionListener){
+        this.setText("Reiniciar jogo");
+        this.addActionListener(actionListener);
+    }
+
+}
