d807b2d8264e8b4557a856ad7e1c460b9f02940d
Raphael San <=>
2025-03-10 14:13:52 -0300

    ajuste no Space

--- src/programacaoOrientadaObjeto/desafios/criandoJogoSudoku/model/Space.java
+++ src/programacaoOrientadaObjeto/desafios/criandoJogoSudoku/model/Space.java
@@ -2,38 +2,37 @@ package programacaoOrientadaObjeto.desafios.criandoJogoSudoku.model;
 
 public class Space {
 
-	private Integer actual;
-	private final int expected;
-	private final boolean fixed;
+    private Integer actual;
+    private final int expected;
+    private final boolean fixed;
 
-	public Space(int expected, boolean fixed) {
-		this.expected = expected;
-		this.fixed = fixed;
-		if (fixed) {
-			actual = expected;
-		}
-	}
 
-	public void setActual(Integer actual) {
-		if (fixed) {
-			return;
-		}
-		this.actual = actual;
-	}
+    public Space(final int expected, final boolean fixed) {
+        this.expected = expected;
+        this.fixed = fixed;
+        if (fixed){
+            actual = expected;
+        }
+    }
 
-	public Integer getActual() {
-		return actual;
-	}
+    public Integer getActual() {
+        return actual;
+    }
 
-	public void clearSpace() {
-		setActual(null);    
-	}
-	public int getExpected() {
-		return expected;
-	}
+    public void setActual(final Integer actual) {
+        if (fixed) return;
+        this.actual = actual;
+    }
 
-	public boolean isFixed() {
-		return fixed;
-	}
+    public void clearSpace(){
+        setActual(null);
+    }
 
+    public int getExpected() {
+        return expected;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
 }
